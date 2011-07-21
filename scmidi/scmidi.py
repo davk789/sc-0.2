aee310419ea0b662391c04ddbdd1a6c8a015804e
davk789 <david.joseph.kendall@gmail.com>
2011-07-21 12:39:27 -0700

    arranging the new project still

---
+++ scmidi/scmidi.py
@@ -0,0 +1,20 @@
+'''
+Created on Jul 21, 2011
+
+@author: davk
+'''
+
+class NoteOnResponder(object):
+    '''
+    Scratch example. Eventually, this file will emulate SuperCollider's 
+    MIDI i/o.
+    '''
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        pass
+        
+if __name__ == "__main__":
+    print "inside scmidi's main"
